2005ddc806c447f755e8d7a78bd6feee1baee9f5
wangtingbiao <5114559@qq.com>
2020-10-12 00:58:14 +0800

    2020.10.12

--- searchform.php
+++ searchform.php
@@ -1,6 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<div class="input-group max-box">
-		<input type="search" name="s" class="forminput" placeholder="<?php echo get_theme_mod('mrw_search_text',__( '来搜点什么', 'mrw-text' ));?>" value="<?php the_search_query(); ?>" />
-		<button type="submit" class="btns formsubmit"><i class="fa fa-search"></i></button>
-	</div>
+    <input type="search" name="s" class="searchinput" placeholder="<?php echo get_theme_mod('mrw_search_text',__( '来搜点什么', 'mrw-text' ));?>" value="<?php the_search_query(); ?>" />
+    <button type="submit" class="searchsubmit"><i class="fa fa-search"></i></button>
 </form>
