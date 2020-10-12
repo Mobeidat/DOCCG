b3c008dac8245b7e31405af6edab82dbc57c7cbe
wangtingbiao <5114559@qq.com>
2020-10-12 18:07:03 +0800

    Update video.php

--- video.php
+++ video.php
@@ -1,124 +1,29 @@
-<!--/**
- * The front page template file.
+<?php
+/**
+ * Video template file.
  *
- * If the user has selected a static page for their homepage, this is what will
- * appear.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * If the category selects this template, it will be displayed as a video list effect.
  *
- * @package Dosth App
+ * @package TingBiao Wang
  */
- -->
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="assets/css/normalize.min.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/share.min.css">
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/jquery.share.min.js"></script>
-    <script src="assets/js/custom.js"></script>
-</head>
-
-<body>
-    <div id="warp">
-        <header id="site-hea">
-            <div class="site-hea-left">
-                <a href="#" id="menu-toggle" class="toggle"><i class="fa fa-bars"></i></a>
-                <h1 class="site-title"><a href="">DOCCG</a><span>2020</span></h1>
+get_header();?>
+<section class="media-box max-width">
+    <article class="media-list">
+        <figure class="media-pic"><a href=""><span class="media-icon"><i class="fa fa-play"></i></span><img src="assets/images/pic.jpg" alt=""></a></figure>
+        <div class="media-content">
+            <h3 class="media-title"><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a></h3>
+            <div class="media-meta">
+                <span class="media-read"><i class="fa fa-eye"></i>156人学习</span>
+                <time class="media-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
             </div>
-            <div class="site-hea-right">
-                <nav id="nav" class="nav">
-                    <ul>
-                        <li><a href="">发现</a></li>
-                        <li><a href="">3D教程</a>
-                            <ul class="sub-menu">
-                                <li><a href="">发现</a></li>
-                                <li><a href="">3D教程</a></li>
-                                <li><a href="">3D资源</a></li>
-                                <li><a href="">网页前端</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="">3D资源</a></li>
-                        <li><a href="">网页前端</a></li>
-                    </ul>
-                </nav>
-                <a href="#" id="search-toggle" class="toggle"><i class="fa fa-search"></i></a>
-            </div>
-        </header>
-        <main id="site-main">
-            <section class="breadcrumbs">
-                <div class="max-width">
-                    <a href="">首页</a><span> &raquo; </span><span>wordpress</span>
-                </div>
-            </section>
-            <section class="media-box max-width">
-                <article class="media-list">
-                    <figure class="media-pic"><a href=""><span class="media-icon"><i class="fa fa-play"></i></span><img src="assets/images/pic.jpg" alt=""></a></figure>
-                    <div class="media-content">
-                        <h3 class="media-title"><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a></h3>
-                        <div class="media-meta">
-                            <span class="media-read"><i class="fa fa-eye"></i>156人学习</span>
-                            <time class="media-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
-                        </div>
-                    </div>
-                </article>
-            </section>
-            <div class="site-next">
-                <a href="">上一页</a>
-                <a href="">1</a>
-                <span>...</span>
-                <a href="">3</a>
-                <a href="">下一页</a>
-            </div>
-        </main>
-        <footer id="site-foo">
-            <section class="site-bottom-ad">
-                <h2>非常棒的主题</h2>
-                <p>你想要这些吗</p>
-                <div class="site-bottom-ad-link"><a href="">查看更多</a></div>
-            </section>
-            <section class="site-bottom">
-                <div class="site-bottom-list bottom-about">
-                    <h1 class="site-title"><a href="">DOCCG</a><span>2020</span></h1>
-                    <p>10多年来，我一直为你们做，并分享我的萝卜的各种程序和实用程序，这是深受互联网用户的欢迎。在网站上，lrepacks.ru你会发现我的最新作品 - 来自埃尔丘帕卡布拉的 repkas 。</p>
-                </div>
-                <div class="site-bottom-list news-posts">
-                    <h3 class="site-bottom-title">快讯</h3>
-                    <div class="news-box">
-                        <article class="news-list">
-                            <h3 class="news-title"><a href="">RaimerSoft RadioMaximus 2.28.3 （重新包装和便携式）</a></h3>
-                            <time class="news-time"><i class="fa fa-calendar-o"></i>2020年3月2日</time>
-                        </article>
-                        <article class="news-list">
-                            <h3 class="news-title"><a href="">RaimerSoft RadioMaximus 2.28.3 （重新包装和便携式）</a></h3>
-                            <time class="news-time"><i class="fa fa-calendar-o"></i>2020年3月2日</time>
-                        </article>
-                        <article class="news-list">
-                            <h3 class="news-title"><a href="">RaimerSoft RadioMaximus 2.28.3 （重新包装和便携式）</a></h3>
-                            <time class="news-time"><i class="fa fa-calendar-o"></i>2020年3月2日</time>
-                        </article>
-                    </div>
-                </div>
-                <div class="site-bottom-list bottom-link">
-                    <h3 class="site-bottom-title">联系我</h3>
-                    <p class="link-icon"><a href=""><i class="fa fa-behance"></i></a><a href=""><i class="fa fa-dribbble"></i></a><a href=""><i class="fa fa-github"></i></a></p>
-                    <p class="link-img"><span>小程序</span><img src="assets/images/buy_02.jpg" alt=""></p>
-                </div>
-            </section>
-            <section class="copyright">
-                <p>© 2016-2020 | DOCCG</p>
-                <div><a href="">使用条款</a><a href="">隐私</a><a href="">帮助</a></div>
-            </section>
-        </footer>
-        <div id="fixed-box">
-            <a href="#" id="bell"><i class="fa fa-bell"></i></a>
-            <a href="#" id="totop"><i class="fa fa-angle-up"></i></a>
         </div>
-    </div>
-</body></html>
+    </article>
+</section>
+<div class="site-next">
+    <a href="">上一页</a>
+    <a href="">1</a>
+    <span>...</span>
+    <a href="">3</a>
+    <a href="">下一页</a>
+</div>
+<?php get_footer();?>
