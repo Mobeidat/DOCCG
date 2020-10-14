d9afa78eed12ec91a2bda3792e0ff8cecb807552
wangtingbiao <5114559@qq.com>
2020-10-14 18:04:12 +0800

    Update single.php

--- single.php
+++ single.php
@@ -9,16 +9,44 @@
 get_header();
 ?>
 <section class="single-box max-width">
+	<?php while ( have_posts() ):the_post();?>
 	<article class="single-post">
 		<header class="single-hea">
 			<div class="single-meta">
 				<time class="single-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
 				<span class="single-read"><i class="fa fa-calendar-o"></i>6分钟阅读</span> <span class="single-cat"><a href="">wordpress</a></span> </div>
-			<h3 class="single-title">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</h3>
+			<?php
+			the_title( '<h3 class="single-title">', '</h3>' );
+			edit_post_link( __( 'Edit', 'mrw-text' ) );
+			?>
 		</header>
-		<main class="single-content"> 最好的WordPress插件。具有内置的WordPress主题功能并非总是一个选择（或最佳选择），因此我们编制了免费和高级的最佳WordPress插件列表 </main>
-		<footer class="single-foo"> 评论 </footer>
+		<main class="single-content">
+			<?php
+			the_content();
+			wp_link_pages();
+			?>
+		</main>
+		<?php
+		$mrw_sin_share_open = get_theme_mod( 'mrw_sin_share_open', 'ture' );
+		$mrw_sin_share = get_theme_mod( 'mrw_sin_share', 'weibo,qzone,qq,wechat' );
+
+		// Single share
+		if ( $mrw_sin_share_open ) {
+			echo '<div class="single-share share-component" data-sites="' . $mrw_sin_share . '"></div>';
+		}
+
+		// Single tags
+		the_tags( '<div class="single-tag">', ' ', '</div>' );
+		?>
+		<footer class="single-foo">
+			<?php
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+			?>
+		</footer>
 	</article>
+	<?php endwhile;?>
 </section>
 <section class="single-pnext"> <a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a> </section>
 <?php get_footer();?>
