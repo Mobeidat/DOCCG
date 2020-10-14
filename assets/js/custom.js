eee91020f7f074e0448b82d2ea4d8acb04c208c7
wangtingbiao <5114559@qq.com>
2020-10-14 18:03:54 +0800

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,49 +1,50 @@
 /*global $*/
 $(document).ready(function () {
-    "use strict";
+	"use strict";
 
-    var WindowTop = 0,
-        SubMenu = $("ul.sub-menu"),
-        Totop = $("a#totop");
+	var SubMenu = $("ul.sub-menu"),
+		Totop = $("a#totop");
 
-    /* Menu toggle */
-    $("a#menu-toggle i").on("click", function (event) {
-        $("nav#nav").slideToggle();
-        if ($(this).hasClass("fa-bars")) {
-            $(this).removeClass("fa-bars").addClass("fa-times").parent("a").css("opacity", "1");
-        } else {
-            $(this).removeClass("fa-times").addClass("fa-bars").parent("a").css("opacity", ".6");
-        }
-        event.stoppropagation();
-    });
+	/* Menu toggle */
+	$("a#menu-toggle i").on("click", function (event) {
+		$("nav#nav").slideToggle();
+		if ($(this).hasClass("fa-bars")) {
+			$(this).removeClass("fa-bars").addClass("fa-times").parent("a").css("opacity", "1");
+		} else {
+			$(this).removeClass("fa-times").addClass("fa-bars").parent("a").css("opacity", ".6");
+		}
+		event.stoppropagation();
+	});
 
-    /* submenu */
-    SubMenu.siblings("a").after('<i class="fa fa-angle-down"></i>');
-    SubMenu.siblings("i").on("click", function (event) {
-        SubMenu.slideToggle();
-        $(this).parent("li").toggleClass("sub-menu-open");
-        if ($(this).hasClass("fa-angle-down")) {
-            $(this).removeClass("fa-angle-down").addClass("fa-angle-up");
-        } else {
-            $(this).removeClass("fa-angle-up").addClass("fa-angle-down");
-        }
-        event.stoppropagation();
-    });
+	/* submenu */
+	SubMenu.siblings("a").after('<i class="fa fa-angle-down"></i>');
+	SubMenu.siblings("i").on("click", function (event) {
+		$(this).next().slideToggle();
+		$(this).parent().toggleClass("sub-menu-open");
+		if ($(this).hasClass("fa-angle-down")) {
+			$(this).removeClass("fa-angle-down").addClass("fa-angle-up");
+			$(this).parent().siblings().removeClass("sub-menu-open").children("ul").slideUp();
+			$(this).parent().siblings().children("i").removeClass("fa-angle-up").addClass("fa-angle-down");
+		} else {
+			$(this).removeClass("fa-angle-up").addClass("fa-angle-down");
+		}
+		event.stoppropagation();
+	});
 
-    /* Totop scroll */
-    $(window).scroll(function () {
-        var Scrolls = $(this).scrollTop();
-        if (Scrolls > 100) {
-            Totop.show();
-        } else {
-            Totop.hide();
-        }
-    });
-    Totop.on("click", function (event) {
-        $("html, body").animate({
-            scrollTop: 0
-        }, "slow");
-        event.stoppropagation();
-    });
+	/* Totop scroll */
+	$(window).scroll(function () {
+		var Scrolls = $(this).scrollTop();
+		if (Scrolls > 100) {
+			Totop.show();
+		} else {
+			Totop.hide();
+		}
+	});
+	Totop.on("click", function (event) {
+		$("html, body").animate({
+			scrollTop: 0
+		}, "slow");
+		event.stoppropagation();
+	});
 
 })
