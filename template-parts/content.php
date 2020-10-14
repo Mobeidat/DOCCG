011944f14218223cf95e234cc0c35f94a3de8023
wangtingbiao <5114559@qq.com>
2020-10-14 18:04:20 +0800

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,48 +1,47 @@
-<article id="post-<?php the_ID(); ?>" class="default-list">
+<article id="post-<?php the_ID(); ?>" class="article-list article-first-list">
 	<?php
 	$doc_list_pic_open = get_theme_mod( 'doc_list_pic_open', 'true' );
 	if ( $doc_list_pic_open ) {
 		if ( has_post_thumbnail() ) {
-			echo '<figure class="default-pic"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
+			echo '<figure class="article-pic"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
 		}
 	}
 	?>
-	<div class="default-content">
+	<div class="article-content">
 		<?php
-		/* card */
+
 		$doc_list_card_open = get_theme_mod( 'doc_list_card_open', 'true' );
+		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
+		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
+		$doc_list_link_text = get_theme_mod( 'doc_list_link_text', __( '查看全文', 'doc-text' ) );
+
+		echo '<div class="article-meta">';
+
+		// Article cat
 		if ( $doc_list_card_open ) {
-			echo '<div class="default-card">';
+			echo '<span class="article-cat">';
 			echo doc_category_foreach();
-			echo '</div>';
+			echo '</span>';
 		}
 
-		/* title */
-		the_title( '<h3 class="default-title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		// Article time
+		if ( $doc_list_time_open ) {
+			echo '<time class="article-time">' . get_the_time( 'Y/m/d' ) . '</time>';
+		}
 
-		/* excerpt */
-		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
+		echo '</div>';
+
+		// Title
+		the_title( '<h3 class="article-title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+
+		// Excerpt
 		if ( $doc_list_excerpt_open ) {
 			the_excerpt();
 		}
+
+		// Link
+		echo '<div class="article-link"><a href="' . get_the_permalink() . '">' . $doc_list_link_text . '</a></div>';
+
 		?>
-		<div class="default-meta">
-			<?php
-			$doc_list_author_open = get_theme_mod( 'doc_list_author_open', 'true' );
-			$doc_list_read_open = get_theme_mod( 'doc_list_read_open', 'true' );
-			$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
-			if ( $doc_list_author_open ) {
-				echo '<span class="default-author">By <a href="">Mr Wang</a></span>';
-			}
-			if ( $doc_list_read_open ) {
-				echo '<span class="default-read">';
-				echo doc_get_reading_time( get_the_content() );
-				echo '</span>';
-			}
-			if ( $doc_list_time_open ) {
-				echo '<time class="default-date">' . get_the_time( 'Y/m/d' ) . '</time>';
-			}
-			?>
-		</div>
 	</div>
 </article>
