6fe37b261e7a840992a347861434eacfbce66ebc
wangtingbiao <5114559@qq.com>
2020-10-14 18:04:09 +0800

    Update search.php

--- search.php
+++ search.php
@@ -8,18 +8,19 @@
  */
 get_header();
 doc_sort_box();
-?>
+if ( have_posts() ):
+	?>
 <section class="article-box max-width">
-	<article class="article-list">
-		<div class="article-content">
-			<div class="article-meta"> <span class="article-cat"><a href="">wordpress</a></span>
-				<time class="article-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
-			</div>
-			<h3 class="article-title"><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a></h3>
-			<p>最好的WordPress插件。具有内置的WordPress主题功能并非总是一个选择（或最佳选择），因此我们编制了免费和高级的最佳WordPress插件列表</p>
-			<div class="article-link"><a href="">查看全文</a></div>
-		</div>
-	</article>
+	<?php
+	while ( have_posts() ): the_post();
+	get_template_part( 'template-parts/content', '' );
+	endwhile;
+	?>
 </section>
-<div class="site-next"> <a href="">上一页</a> <a href="">1</a> <span>...</span> <a href="">3</a> <a href="">下一页</a> </div>
-<?php get_footer();?>
+<?php
+get_template_part( 'template-parts/content', 'next' );
+else :
+	get_template_part( 'template-parts/content', 'none' );
+endif;
+get_footer();
+?>
