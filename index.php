37c0c9f196c1ec268825036975fe475c0fd70460
wangtingbiao <5114559@qq.com>
2020-10-14 18:04:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Post page template file.
+ *
+ * If the article format is not selected, this default template will be displayed.
+ *
+ * @package TingBiao Wang
+ */
+get_header();
+?>
+<section class="banner" style="background-image: url(assets/images/pic.jpg)">
+	<div class="banner-content max-width">
+		<h2>fds</h2>
+		<p>aa</p>
+	</div>
+</section>
+<?php get_footer();?>
