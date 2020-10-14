e52182bd9a7428e5e58561c36efdc9c38b622ce2
wangtingbiao <5114559@qq.com>
2020-10-14 18:04:16 +0800

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,1 +1,3 @@
-<p><?php echo __("Sorry, no search criteria are met. Please try some other keywords.","doc-text");?></p>
+<section class="article-box max-width">
+	<p><?php echo __("Sorry, no search criteria are met. Please try some other keywords.","doc-text");?></p>
+</section>
