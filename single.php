cf365620ab5b762a3c6543e22dbebbae71f32b32
wangtingbiao <5114559@qq.com>
2020-10-15 13:39:16 +0800

    Update single.php

--- single.php
+++ single.php
@@ -8,19 +8,36 @@
  */
 get_header();
 ?>
-<section class="single-box max-width">
+<section class="single-box max-width" role="article" itemscope itemtype="http://schema.org/Article">
 	<?php while ( have_posts() ):the_post();?>
 	<article class="single-post">
 		<header class="single-hea">
 			<div class="single-meta">
-				<time class="single-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
-				<span class="single-read"><i class="fa fa-calendar-o"></i>6分钟阅读</span> <span class="single-cat"><a href="">wordpress</a></span> </div>
+				<?php
+				// Time
+				echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
+
+				// Read time
+				echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
+				echo doc_get_reading_time( $content );
+				echo '</span>';
+
+				// Comment
+				echo '<span class="single-comment" itemprop="comment"><i class="fa fa-comment"></i>' . get_comments_number() . '</span>';
+
+				// Category
+				echo '<span class="single-cat" itemprop="keywords">';
+				doc_category_foreach();
+				echo '</span> ';
+
+				?>
+			</div>
 			<?php
-			the_title( '<h3 class="single-title">', '</h3>' );
+			the_title( '<h3 class="single-title" itemprop="headline">', '</h3>' );
 			edit_post_link( __( 'Edit', 'mrw-text' ) );
 			?>
 		</header>
-		<main class="single-content">
+		<main class="single-content" itemprop="articleBody">
 			<?php
 			the_content();
 			wp_link_pages();
