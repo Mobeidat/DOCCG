66d18cd0c6f4881cd85bc6ba23bc73fb4921347c
wangtingbiao <5114559@qq.com>
2020-10-15 13:38:56 +0800

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,9 +1,9 @@
-<article id="post-<?php the_ID(); ?>" class="article-list article-first-list">
+<article id="post-<?php the_ID(); ?>" class="article-list article-first-list" itemscope itemtype="http://schema.org/article">
 	<?php
 	$doc_list_pic_open = get_theme_mod( 'doc_list_pic_open', 'true' );
 	if ( $doc_list_pic_open ) {
 		if ( has_post_thumbnail() ) {
-			echo '<figure class="article-pic"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
+			echo '<figure class="article-pic" itemprop="image"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
 		}
 	}
 	?>
@@ -19,20 +19,20 @@
 
 		// Article cat
 		if ( $doc_list_card_open ) {
-			echo '<span class="article-cat">';
+			echo '<span class="article-cat" itemprop="keywords">';
 			echo doc_category_foreach();
 			echo '</span>';
 		}
 
 		// Article time
 		if ( $doc_list_time_open ) {
-			echo '<time class="article-time">' . get_the_time( 'Y/m/d' ) . '</time>';
+			echo '<time class="article-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished">' . get_the_time( 'Y-m-d' ) . '</time>';
 		}
 
 		echo '</div>';
 
 		// Title
-		the_title( '<h3 class="article-title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		the_title( '<h3 class="article-title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 
 		// Excerpt
 		if ( $doc_list_excerpt_open ) {
@@ -40,7 +40,7 @@
 		}
 
 		// Link
-		echo '<div class="article-link"><a href="' . get_the_permalink() . '">' . $doc_list_link_text . '</a></div>';
+		echo '<div class="article-link" itemprop="url" ><a href="' . get_the_permalink() . '">' . $doc_list_link_text . '</a></div>';
 
 		?>
 	</div>
