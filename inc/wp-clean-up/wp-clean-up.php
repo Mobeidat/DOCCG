4ae59d636c478e162dd48094923d02b481ada3fe
wangtingbiao <5114559@qq.com>
2020-10-16 23:02:07 +0800

    Update wp-clean-up.php

--- inc/wp-clean-up/wp-clean-up.php
+++ inc/wp-clean-up/wp-clean-up.php
@@ -1,4 +1,15 @@
 <?php
+/*
+Plugin Name: WP Clean Up
+Plugin URI: http://boliquan.com/wp-clean-up/
+Description: WP Clean Up can help us to clean up the wordpress database by removing "revision" "draft" "auto draft" "moderated comments" "spam comments" "trash comments" "orphan postmeta" "orphan commentmeta" "orphan relationships" "dashboard transient feed". It allows you to optimize your WordPress database without phpMyAdmin.
+Version: 1.2.3
+Author: BoLiQuan
+Author URI: http://boliquan.com/
+Text Domain: WP-Clean-Up
+Domain Path: /lang
+*/
+
 function load_wp_clean_up_lang(){
 	$currentLocale = get_locale();
 	if(!empty($currentLocale)){
