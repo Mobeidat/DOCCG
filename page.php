1042ced3c2ef354f0b4eeac5da111ba09915531b
wangtingbiao <5114559@qq.com>
2020-10-16 23:01:56 +0800

    Update page.php

--- page.php
+++ page.php
@@ -8,26 +8,33 @@
  */
 get_header();
 ?>
-<section class="single-box" role="article" itemscope itemtype="http://schema.org/Article">
-    <?php while ( have_posts() ):the_post();?>
-    <article class="single-post">
-        <header class="single-hea">
-            <?php the_title( '<h3 class="single-title" itemprop="headline">', '</h3>' );?>
-        </header>
-        <main class="single-content" itemprop="articleBody">
-            <?php
+<section class="single-box max-width" role="article" itemscope itemtype="http://schema.org/Article">
+	<?php while ( have_posts() ):the_post();?>
+	<article class="single-posts">
+		<header class="single-hea">
+			<?php
+			// Edit
+			echo '<div class="single-meta">';
+			edit_post_link( __( 'Edit', 'doctext' ) );
+			echo '</div>';
+
+			the_title( '<h3 class="single-title" itemprop="headline">', '</h3>' );
+			?>
+		</header>
+		<main class="single-content" itemprop="articleBody">
+			<?php
 			the_content();
 			wp_link_pages();
 			?>
-        </main>
-        <?php
+		</main>
+		<?php
 		echo '<footer class="single-foo" itemprop="comment"><div class="single-foo-box max-width"><a class="single-foo-close comment-toggle"><span>' . __( 'Comment', 'doc-text' ) . '</span><i class="fa fa-close"></i></a>';
 		if ( comments_open() || get_comments_number() ) {
 			comments_template();
 		}
 		echo '</div></footer>';
 		?>
-    </article>
-    <?php endwhile;?>
+	</article>
+	<?php endwhile;?>
 </section>
 <?php get_footer();?>
