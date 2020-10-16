7510af76acd04e469f6203b491a2a7360d4f8d87
wangtingbiao <5114559@qq.com>
2020-10-16 17:11:03 +0800

    Update single.php

--- single.php
+++ single.php
@@ -13,26 +13,33 @@ get_header();
 	<article class="single-post">
 		<header class="single-hea">
 			<?php
+			$doc_single_top_category_open = get_theme_mod( 'doc_single_top_category_open', 'ture' );
+			$doc_single_top_meta_open = get_theme_mod( 'doc_single_top_meta_open', 'ture' );
 
 			// Category
-			echo '<div class="single-category" itemprop="keywords">';
-			doc_category_foreach();
-			echo '</div> ';
-			
-			// Time / Read time / Comment
-			echo '<div class="single-meta">';
-			echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
-			echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
-			echo doc_get_reading_time( $content );
-			echo '</span>';
-			echo '<span class="single-comment" itemprop="comment"><i class="fa fa-comment"></i><a class="comment-toggle">' . get_comments_number() . '</a></span>';
-			echo '</div>';
+			if ( $doc_single_top_category_open ) {
+				echo '<div class="single-category" itemprop="keywords">';
+				doc_category_foreach();
+				echo '</div> ';
+			}
+
+			// Time / Read time / Comment / Edit
+			if ( $doc_single_top_meta_open ) {
+				echo '<div class="single-meta">';
+				echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
+
+				echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
+				echo doc_get_reading_time( $content );
+				echo '</span>';
+
+				echo '<a class="comment-toggle single-comment" itemprop="comment""><i class="fa fa-comment"></i>' . get_comments_number() . '</a>';
+
+				edit_post_link( __( 'Edit', 'doctext' ) );
+				echo '</div>';
+			}
 
 			// Title
 			the_title( '<h3 class="single-title" itemprop="headline">', '</h3>' );
-
-			// Edit
-			edit_post_link( __( 'Edit', 'doctext' ) );
 			?>
 		</header>
 		<main class="single-content" itemprop="articleBody">
@@ -42,22 +49,25 @@ get_header();
 			?>
 		</main>
 		<?php
-		echo '<div class="single-meta">';
-
-		// Time
-		echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
-
-		// Read time
-		echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
-		echo doc_get_reading_time( $content );
-		echo '</span>';
-
-		// Comment
-		echo '<span class="single-comment" itemprop="comment"><i class="fa fa-comment"></i><a class="comment-toggle">' . get_comments_number() . '</a></span>';
-		echo '</div>';
-
+		$doc_single_bottom_meta_open = get_theme_mod( 'doc_single_bottom_meta_open', 'ture' );
 		$doc_sin_share_open = get_theme_mod( 'doc_sin_share_open', 'ture' );
 		$doc_sin_share = get_theme_mod( 'doc_sin_share', 'weibo,qzone,qq,wechat' );
+		$doc_sin_tag_open = get_theme_mod( 'doc_sin_tag_open', 'ture' );
+
+		// Time / Read time / Comment / Edit
+		if ( $doc_single_bottom_meta_open ) {
+			echo '<div class="single-meta">';
+			echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
+
+			echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
+			echo doc_get_reading_time( $content );
+			echo '</span>';
+
+			echo '<a class="comment-toggle single-comment" itemprop="comment""><i class="fa fa-comment"></i>' . get_comments_number() . '</a>';
+
+			edit_post_link( __( 'Edit', 'doctext' ) );
+			echo '</div>';
+		}
 
 		// Single share
 		if ( $doc_sin_share_open ) {
@@ -65,7 +75,9 @@ get_header();
 		}
 
 		// Single tags
-		the_tags( '<div class="single-tag">', ' ', '</div>' );
+		if ( $doc_sin_tag_open ) {
+			the_tags( '<div class="single-tag">', ' ', '</div>' );
+		}
 
 		// Comment
 		echo '<footer class="single-foo" itemprop="comment"><div class="single-foo-box max-width"><a class="single-foo-close comment-toggle"><span>' . __( 'Comment', 'doc-text' ) . '</span><i class="fa fa-close"></i></a>';
@@ -77,5 +89,14 @@ get_header();
 	</article>
 	<?php endwhile;?>
 </section>
-<section class="single-pnext"> <a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a> </section>
+<section class="single-pnext">
+	<?php
+	echo '<p>';
+	previous_post_link( '<i class="fa fa-arrow-left"></i> %link', ' %title', true );
+	echo '</p>';
+	echo '<p>';
+	next_post_link( '%link <i class="fa fa-arrow-right"></i>', '%title', true );
+	echo '</p>';
+	?>
+</section>
 <?php get_footer();?>
