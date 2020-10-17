eb800363fee4fb12363eaf86a75b910e6a6ad518
wangtingbiao <5114559@qq.com>
2020-10-18 01:01:03 +0800

    Update content-next.php

--- template-parts/content-next.php
+++ template-parts/content-next.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Pagination template file.
+ *
+ * Use digital paging by default.
+ *
+ * @package TingBiao Wang
+ */
 if ( $wp_query->max_num_pages > 1 ) {
 	echo '<div class="site-next">';
-	echo paginate_links( array( 'prev_text' => '上一页', 'next_text' => '下一页', 'mid_size' => 1 ) );
+	echo paginate_links( array( 'prev_text' => __( '上一页', 'doc-text' ), 'next_text' => __( '下一页', 'doc-text' ), 'mid_size' => 1 ) );
 	/*if ( !is_paged() ) {
-		next_posts_link( '下一页' );
+		next_posts_link( __( '下一页', 'doc-text' ) );
 	} else {}*/
 	echo '</div>';
 }
