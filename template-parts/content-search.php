60bcbf9d0ff9a068034833cab2128a934ed44c2c
wangtingbiao <5114559@qq.com>
2020-10-18 01:00:57 +0800

    Update content-search.php

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -1,24 +1,29 @@
+<?php
+/**
+ * Search list template file.
+ *
+ * Search page setting independent search list template.
+ *
+ * @package TingBiao Wang
+ */
+?>
 <article id="post-<?php the_ID(); ?>" class="article-list" itemscope itemtype="http://schema.org/article">
 	<div class="article-content">
 		<?php
-
-		$doc_list_card_open = get_theme_mod( 'doc_list_card_open', 'true' );
-		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
-		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
-
 		echo '<div class="article-meta">';
 
 		// Article cat
-		if ( $doc_list_card_open ) {
+		$doc_list_category_open = get_theme_mod( 'doc_list_category_open', 'true' );
+		if ( $doc_list_category_open ) {
 			echo '<span class="article-cat" itemprop="keywords">';
-			echo doc_category_foreach();
+			echo doc_get_category();
 			echo '</span>';
 		}
 
 		// Article time
-		if ( $doc_list_time_open ) {
-			echo '<time class="article-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished">' . get_the_time( 'Y-m-d' ) . '</time>';
-		}
+		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
+		if ( $doc_list_time_open ): echo '<time class="article-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished">' . get_the_time( 'Y-m-d' ) . '</time>';
+		endif;
 
 		echo '</div>';
 
@@ -26,10 +31,10 @@
 		the_title( '<h3 class="article-title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 
 		// Excerpt
+		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
 		if ( $doc_list_excerpt_open ) {
 			the_excerpt();
 		}
-
 		?>
 	</div>
 </article>
