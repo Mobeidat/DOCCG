f9dde04a9650a7c4a2c6d851ea5255d746e0827e
wangtingbiao <5114559@qq.com>
2020-10-18 01:00:59 +0800

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * No post list template file.
+ *
+ * Use this list template when there are no articles.
+ *
+ * @package TingBiao Wang
+ */
+?>
 <section class="article-box max-width">
-	<p><?php echo __("Sorry, no search criteria are met. Please try some other keywords.","doc-text");?></p>
+	<p>
+		<?php _e("Sorry, no search criteria are met. Please try some other keywords.","doc-text");?>
+	</p>
 </section>
