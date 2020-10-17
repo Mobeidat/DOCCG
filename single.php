6f0a9814f2cf46493bebef3cbadbdbfe41a81c93
wangtingbiao <5114559@qq.com>
2020-10-18 01:01:08 +0800

    Update single.php

--- single.php
+++ single.php
@@ -13,29 +13,19 @@ get_header();
 	<article class="single-posts">
 		<header class="single-hea">
 			<?php
-			$doc_single_top_category_open = get_theme_mod( 'doc_single_top_category_open', 'ture' );
-			$doc_single_top_meta_open = get_theme_mod( 'doc_single_top_meta_open', 'ture' );
 
 			// Category
-			if ( $doc_single_top_category_open ) {
+			$doc_sin_top_category_open = get_theme_mod( 'doc_sin_top_category_open', 'ture' );
+			if ( $doc_sin_top_category_open ) {
 				echo '<div class="single-category" itemprop="keywords">';
-				doc_category_foreach();
+				doc_get_category();
 				echo '</div> ';
 			}
 
 			// Time / Read time / Comment / Edit
-			echo '<div class="single-meta">';
-			if ( $doc_single_top_meta_open ) {
-				echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
-
-				echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
-				echo doc_get_reading_time( $content );
-				echo '</span>';
-
-				echo '<a class="comment-toggle single-comment" itemprop="comment""><i class="fa fa-comment"></i>' . get_comments_number() . '</a>';
-			}
-			edit_post_link( __( 'Edit', 'doctext' ) );
-			echo '</div>';
+			$doc_sin_top_meta_open = get_theme_mod( 'doc_sin_top_meta_open', 'ture' );
+			if ( $doc_sin_top_meta_open ): doc_get_single_meta();
+			endif;
 
 			// Title
 			the_title( '<h3 class="single-title" itemprop="headline">', '</h3>' );
@@ -48,35 +38,22 @@ get_header();
 			?>
 		</main>
 		<?php
-		$doc_single_bottom_meta_open = get_theme_mod( 'doc_single_bottom_meta_open', 'ture' );
-		$doc_sin_share_open = get_theme_mod( 'doc_sin_share_open', 'ture' );
-		$doc_sin_share = get_theme_mod( 'doc_sin_share', 'weibo,qq,wechat,tencent,qzone,facebook,twitter,google' );
-		$doc_sin_tag_open = get_theme_mod( 'doc_sin_tag_open', 'ture' );
 
 		// Time / Read time / Comment / Edit
-		if ( $doc_single_bottom_meta_open ) {
-			echo '<div class="single-meta">';
-			echo '<time class="single-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
-
-			echo '<span class="single-read"><i class="fa fa-calendar-o"></i>';
-			echo doc_get_reading_time( $content );
-			echo '</span>';
-
-			echo '<a class="comment-toggle single-comment" itemprop="comment""><i class="fa fa-comment"></i>' . get_comments_number() . '</a>';
-
-			edit_post_link( __( 'Edit', 'doctext' ) );
-			echo '</div>';
-		}
+		$doc_sin_bottom_meta_open = get_theme_mod( 'doc_sin_bottom_meta_open', 'ture' );
+		if ( $doc_sin_bottom_meta_open ): doc_get_single_meta();
+		endif;
 
 		// Single share
-		if ( $doc_sin_share_open ) {
-			echo '<div class="single-share share-component" data-sites="' . $doc_sin_share . '"></div>';
-		}
+		$doc_sin_share_open = get_theme_mod( 'doc_sin_share_open', 'ture' );
+		$doc_sin_share = get_theme_mod( 'doc_sin_share', 'weibo,qq,wechat,tencent,qzone,facebook,twitter,google' );
+		if ( $doc_sin_share_open ): echo '<div class="single-share share-component" data-sites="' . $doc_sin_share . '"></div>';
+		endif;
 
 		// Single tags
-		if ( $doc_sin_tag_open ) {
-			the_tags( '<div class="single-tag">', ' ', '</div>' );
-		}
+		$doc_sin_tag_open = get_theme_mod( 'doc_sin_tag_open', 'ture' );
+		if ( $doc_sin_tag_open ): the_tags( '<div class="single-tag">', ' ', '</div>' );
+		endif;
 
 		// Comment
 		echo '<footer class="single-foo single-foo-fixed" itemprop="comment"><div class="single-foo-box max-width"><a class="single-foo-close comment-toggle"><span>' . __( 'Comment', 'doc-text' ) . '</span><i class="fa fa-close"></i></a>';
