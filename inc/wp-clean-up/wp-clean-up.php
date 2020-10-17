48fa66855a0977a881b684bdcbd9e9bd3ea54ffe
wangtingbiao <5114559@qq.com>
2020-10-18 01:15:17 +0800

    Update wp-clean-up.php

--- inc/wp-clean-up/wp-clean-up.php
+++ inc/wp-clean-up/wp-clean-up.php
@@ -10,23 +10,25 @@ Text Domain: WP-Clean-Up
 Domain Path: /lang
 */
 
-function load_wp_clean_up_lang(){
+function load_wp_clean_up_lang() {
 	$currentLocale = get_locale();
-	if(!empty($currentLocale)){
-		$moFile = dirname(__FILE__) . "/lang/wp-clean-up-" . $currentLocale . ".mo";
-		if(@file_exists($moFile) && is_readable($moFile)) load_textdomain('WP-Clean-Up',$moFile);
+	if ( !empty( $currentLocale ) ) {
+		$moFile = dirname( __FILE__ ) . "/lang/wp-clean-up-" . $currentLocale . ".mo";
+		if ( @file_exists( $moFile ) && is_readable( $moFile ) )load_textdomain( 'WP-Clean-Up', $moFile );
 	}
 }
-add_filter('init','load_wp_clean_up_lang');
+add_filter( 'init', 'load_wp_clean_up_lang' );
 
-function wp_clean_up_settings_link($action_links,$plugin_file){
-	if($plugin_file==plugin_basename(__FILE__)){
-		$wcu_settings_link = '<a href="options-general.php?page=' . dirname(plugin_basename(__FILE__)) . '/wp_clean_up_admin.php">' . __("Settings") . '</a>';
-		array_unshift($action_links,$wcu_settings_link);
+function wp_clean_up_settings_link( $action_links, $plugin_file ) {
+	if ( $plugin_file == plugin_basename( __FILE__ ) ) {
+		$wcu_settings_link = '<a href="options-general.php?page=' . dirname( plugin_basename( __FILE__ ) ) . '/wp_clean_up_admin.php">' . __( "Settings" ) . '</a>';
+		array_unshift( $action_links, $wcu_settings_link );
 	}
 	return $action_links;
 }
-add_filter('plugin_action_links','wp_clean_up_settings_link',10,2);
+add_filter( 'plugin_action_links', 'wp_clean_up_settings_link', 10, 2 );
 
-if(is_admin()){require_once('wp_clean_up_admin.php');}
+if ( is_admin() ) {
+	require_once( 'wp_clean_up_admin.php' );
+}
 ?>
