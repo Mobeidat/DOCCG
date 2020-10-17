31b615e41b2e96805d549ecab8b06266661b0535
wangtingbiao <5114559@qq.com>
2020-10-18 01:01:01 +0800

    Update content-video.php

--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -1,16 +1,27 @@
-<article class="topic-list">
-	<div class="topic-box">
-		<div class="topic-con">
-			<?php
-			the_title( '<h3 class="topic-title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
-			doc_topic_meta();
-			doc_topic_tag();
-			the_excerpt();
-			doc_topic_link();
-			?>
-		</div>
-		<?php if ( has_post_thumbnail() ) : ?>
-		<figure class="topic-pic" style="background-image: url(<?php echo the_post_thumbnail_url();?>)"><a href="<?php the_permalink();?>"></a></figure>
-		<?php endif;?>
+<?php
+/**
+ * Video list template file.
+ *
+ * Selecting the current template by category will display a three-column video list template.
+ *
+ * @package TingBiao Wang
+ */
+?>
+<article id="post-<?php the_ID(); ?>" class="media-list">
+	<?php
+	if ( has_post_thumbnail() ) {
+		echo '<figure class="media-pic" itemprop="image"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
+	}
+	?>
+	<div class="media-content">
+		<?php
+		the_title( '<h3 class="media-title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
+		echo '<div class="media-meta">';
+		echo '<span class="media-read"><i class="fa fa-eye"></i>';
+		echo doc_get_post_views( $post->ID );
+		echo '人学习</span>';
+		echo '<time class="media-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished"><i class="fa fa-calendar-o"></i>' . get_the_time( 'Y-m-d' ) . '</time>';
+		echo '</div>';
+		?>
 	</div>
 </article>
