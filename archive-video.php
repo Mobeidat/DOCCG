b766966295f446575d9fd9a8272d1570c3acaa98
wangtingbiao <5114559@qq.com>
2020-10-18 01:01:31 +0800

    Update archive-video.php

--- archive-video.php
+++ archive-video.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * Template Name: Video[视频]
+ *
  * Video template file.
  *
  * If the category selects this template, it will be displayed as a video list effect.
@@ -7,17 +9,20 @@
  * @package TingBiao Wang
  */
 get_header();
-?>
+doc_sort_box();
+if ( have_posts() ):
+	?>
 <section class="media-box max-width">
-	<article class="media-list">
-		<figure class="media-pic"><a href=""><span class="media-icon"><i class="fa fa-play"></i></span><img src="assets/images/pic.jpg" alt=""></a></figure>
-		<div class="media-content">
-			<h3 class="media-title"><a href="">50+最好的WordPress插件免费和高级50+最好的WordPress插件免费和高级</a></h3>
-			<div class="media-meta"> <span class="media-read"><i class="fa fa-eye"></i>156人学习</span>
-				<time class="media-time"><i class="fa fa-calendar-o"></i>2020-09-30</time>
-			</div>
-		</div>
-	</article>
+	<?php
+	while ( have_posts() ): the_post();
+	get_template_part( 'template-parts/content', 'video' );
+	endwhile;
+	?>
 </section>
-<div class="site-next"> <a href="">上一页</a> <a href="">1</a> <span>...</span> <a href="">3</a> <a href="">下一页</a> </div>
-<?php get_footer();?>
+<?php
+get_template_part( 'template-parts/content', 'next' );
+else :
+	get_template_part( 'template-parts/content', 'none' );
+endif;
+get_footer();
+?>
