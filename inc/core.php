432eb83ed9aad2c524a04d418618bfba53eb9b0c
wangtingbiao <5114559@qq.com>
2020-10-18 01:01:16 +0800

    Update core.php

--- inc/core.php
+++ inc/core.php
@@ -1,25 +1,58 @@
 <?php
+/**
+ * Important core code.
+ *
+ * Login in the background to register and display the logo link
+ * Login in the background to register and display the logo name
+ * Customize admin footer text
+ * Protect your website from malicious requests
+ * Only show articles from current user
+ * Kill the admin nag
+ * Remove nofollow from comments
+ * There is only one search result directly enter
+ * Remove Query String from Static Resources
+ * Remove Emojis
+ * Remove Shortlink
+ * Disable Embed
+ * Disable XML-RPC
+ * Remove RSD Link
+ * Hide Version
+ * Remove WLManifest Link
+ * Disable Self Pingback
+ * Disable Dashicons in Front-end
+ * Disable Contact Form 7 CSS/JS on Every Page
+ *
+ * @package TingBiao Wang
+ */
 
-/* Login in the background to register and display the logo link */
+/**
+ * Login in the background to register and display the logo link
+ */
 function doc_login_logo_url() {
 	return home_url();
 }
 add_filter( 'login_headerurl', 'doc_login_logo_url' );
 
-/* Login in the background to register and display the logo name */
+/**
+ * Login in the background to register and display the logo name
+ */
 function doc_login_logo_url_title() {
 	$doc_login_logo_url_title_a = get_bloginfo( 'name' );
 	return $doc_login_logo_url_title_a;
 }
 add_filter( 'login_headertitle', 'doc_login_logo_url_title' );
 
-/* Customize admin footer text */
+/**
+ * Customize admin footer text
+ */
 function doc_custom_admin_footer() {
 	echo '<a href="' . get_home_url() . '">' . get_bloginfo( 'name' ) . '</a>';
 }
 add_filter( 'admin_footer_text', 'doc_custom_admin_footer' );
 
-/* Protect your website from malicious requests */
+/**
+ * Protect your website from malicious requests
+ */
 global $user_ID;
 if ( $user_ID ) {
 	if ( !current_user_can( 'administrator' ) ) {
@@ -36,7 +69,9 @@ if ( $user_ID ) {
 	}
 }
 
-/* Only show articles from current user */
+/**
+ * Only show articles from current user
+ */
 function doc_check_user_role() {
 	global $current_user;
 	if ( $current_user->roles[ 0 ] != 'administrator' ) {
@@ -58,13 +93,17 @@ function doc_check_user_role() {
 }
 add_action( 'init', 'doc_check_user_role' );
 
-/* Kill the admin nag */
+/**
+ * Kill the admin nag
+ */
 if ( !current_user_can( 'edit_users' ) ) {
 	add_action( 'init', create_function( '$a', "remove_action('init', 'wp_version_check');" ), 2 );
 	add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
 }
 
-/* Remove junk from head */
+/**
+ * Remove junk from head
+ */
 remove_action( 'wp_head', 'rsd_link' );
 remove_action( 'wp_head', 'wp_generator' );
 remove_action( 'wp_head', 'feed_links', 2 );
@@ -75,7 +114,9 @@ remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 
-/* Remove nofollow from comments */
+/**
+ * Remove nofollow from comments
+ */
 function doc_xwp_dofollow( $str ) {
 	$str = preg_replace(
 		'~<a ([^>]*)\s*(["|\']{1}\w*)\s*nofollow([^>]*)>~U',
@@ -88,8 +129,10 @@ add_filter( 'post_comments_link', 'doc_xwp_dofollow' );
 add_filter( 'comment_reply_link', 'doc_xwp_dofollow' );
 add_filter( 'comment_text', 'doc_xwp_dofollow' );
 
-/* There is only one search result directly enter */
-function doc_single_result() {
+/**
+ * There is only one search result directly enter
+ */
+function doc_sin_result() {
 	if ( is_search() ) {
 		global $wp_query;
 		if ( $wp_query->post_count == 1 ) {
@@ -97,9 +140,11 @@ function doc_single_result() {
 		}
 	}
 }
-add_action( 'template_redirect', 'doc_single_result' );
+add_action( 'template_redirect', 'doc_sin_result' );
 
-/* Remove Query String from Static Resources */
+/**
+ * Remove Query String from Static Resources
+ */
 function doc_remove_cssjs_ver( $src ) {
 	if ( strpos( $src, '?ver=' ) )
 		$src = remove_query_arg( 'ver', $src );
@@ -108,34 +153,50 @@ function doc_remove_cssjs_ver( $src ) {
 add_filter( 'style_loader_src', 'doc_remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'doc_remove_cssjs_ver', 10, 2 );
 
-/* Remove Emojis */
+/**
+ * Remove Emojis
+ */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-/* Remove Shortlink */
+/**
+ * Remove Shortlink
+ */
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
-/* Disable Embed */
+/**
+ * Disable Embed
+ */
 function doc_disable_embed() {
 	wp_dequeue_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'doc_disable_embed' );
 
-/* Disable XML-RPC */
+/**
+ * Disable XML-RPC
+ */
 add_filter( 'xmlrpc_enabled', '__return_false' );
 
-/* Remove RSD Link */
+/**
+ * Remove RSD Link
+ */
 remove_action( 'wp_head', 'rsd_link' );
 
-/* Hide Version */
+/**
+ * Hide Version
+ */
 remove_action( 'wp_head', 'wp_generator' );
 
-/* Remove WLManifest Link */
+/**
+ * Remove WLManifest Link
+ */
 remove_action( 'wp_head', 'wlwmanifest_link' );
 
-/* Disable Self Pingback */
+/**
+ * Disable Self Pingback
+ */
 function doc_disable_pingback( & $links ) {
 	foreach ( $links as $l => $link )
 		if ( 0 === strpos( $link, get_option( 'home' ) ) )
@@ -143,7 +204,9 @@ function doc_disable_pingback( & $links ) {
 }
 add_action( 'pre_ping', 'doc_disable_pingback' );
 
-/* Disable Dashicons in Front-end */
+/**
+ * Disable Dashicons in Front-end
+ */
 function doc_docs_dequeue_dashicon() {
 	if ( current_user_can( 'update_core' ) ) {
 		return;
@@ -152,6 +215,8 @@ function doc_docs_dequeue_dashicon() {
 }
 add_action( 'wp_enqueue_scripts', 'doc_docs_dequeue_dashicon' );
 
-/* Disable Contact Form 7 CSS/JS on Every Page */
+/**
+ * Disable Contact Form 7 CSS/JS on Every Page
+ */
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
