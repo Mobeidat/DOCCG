c7fd79d6c20f4297178d0fd69731feed1d1fc829
wangtingbiao <5114559@qq.com>
2020-10-22 00:58:30 +0800

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,7 +9,7 @@
 ?>
 <article id="post-<?php the_ID(); ?>" class="article-list article-first-list" itemscope itemtype="http://schema.org/article">
 	<?php
-	$doc_list_pic_open = get_theme_mod( 'doc_list_pic_open', 'true' );
+	$doc_list_pic_open = get_theme_mod( 'doc_list_pic_open', 1 );
 	if ( $doc_list_pic_open ) {
 		if ( has_post_thumbnail() ) {
 			echo '<figure class="article-pic" itemprop="image"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail() . '</a></figure>';
@@ -19,10 +19,10 @@
 	<div class="article-content">
 		<?php
 
-		$doc_list_category_open = get_theme_mod( 'doc_list_category_open', 'true' );
-		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
-		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
-		$doc_list_link_text_open = get_theme_mod( 'doc_list_link_text_open', 'true' );
+		$doc_list_category_open = get_theme_mod( 'doc_list_category_open', 1 );
+		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 1 );
+		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 1 );
+		$doc_list_link_text_open = get_theme_mod( 'doc_list_link_text_open', 1 );
 		$doc_list_link_text = get_theme_mod( 'doc_list_link_text', __( 'Read more', 'doc-text' ) );
 
 		echo '<div class="article-meta">';
