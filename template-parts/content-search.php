1d1b236c30b1b70c749ba465a3960232141fe132
wangtingbiao <5114559@qq.com>
2020-10-22 00:58:33 +0800

    Update content-search.php

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -13,7 +13,7 @@
 		echo '<div class="article-meta">';
 
 		// Article cat
-		$doc_list_category_open = get_theme_mod( 'doc_list_category_open', 'true' );
+		$doc_list_category_open = get_theme_mod( 'doc_list_category_open', 1 );
 		if ( $doc_list_category_open ) {
 			echo '<span class="article-cat" itemprop="keywords">';
 			echo doc_get_category();
@@ -21,7 +21,7 @@
 		}
 
 		// Article time
-		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 'true' );
+		$doc_list_time_open = get_theme_mod( 'doc_list_time_open', 1 );
 		if ( $doc_list_time_open ): echo '<time class="article-time" datetime="' . get_the_time( 'Y-m-d A G:i:s' ) . '" itemprop="datePublished">' . get_the_time( 'Y-m-d' ) . '</time>';
 		endif;
 
@@ -31,7 +31,7 @@
 		the_title( '<h3 class="article-title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 
 		// Excerpt
-		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 'true' );
+		$doc_list_excerpt_open = get_theme_mod( 'doc_list_excerpt_open', 1 );
 		if ( $doc_list_excerpt_open ) {
 			the_excerpt();
 		}
