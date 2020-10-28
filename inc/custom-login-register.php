d3ec26f5d24e5201ff563ff33b92fa190770a29f
wangtingbiao <5114559@qq.com>
2020-10-29 00:54:04 +0800

    Update custom-login-register.php

--- inc/custom-login-register.php
+++ inc/custom-login-register.php
@@ -1,6 +1,8 @@
 <?php
 
-/* Beautify the default login/register page */
+/**
+ * Beautify the default login/register page
+ */
 function doc_default_login_register() {
 	?>
 <style type="text/css">
@@ -8,74 +10,90 @@ html, body {
 	height: auto!important;
 	min-height: 100vh!important;
 }
-.login {
-	display: flex;
-	justify-content: center;
-	align-items: center;
+<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+$logo = wp_get_attachment_image_src( $custom_logo_id, 'full' );
+if ( has_custom_logo() ) {
+echo'.login div#login h1 a {
+background-image: url('.esc_url( $logo[ 0 ] ).');
+background-position: center;
+background-repeat: no-repeat;
+background-size: auto 100%;
+}
+';
+}
+else {
+echo'.login div#login h1 a {
+text-indent: 0;
+background-image: none;
+}
+';
+}
+?> .login {
+display: flex;
+justify-content: center;
+align-items: center;
 background-image: url(<?php echo get_stylesheet_directory_uri().'/assets/images/bg.svg';
 ?>);
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-	background-color: #3C80FF;
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+background-color: #232323;
 }
 .login div#login {
 	display: flex;
 	justify-content: space-between;
 	flex-wrap: wrap;
 	width: auto;
-	max-width: 320px;
-	margin: 32px;
-	padding: 16px;
-	color: #666;
-	text-align: center;
+	max-width: 375px;
+	overflow: hidden;
+	margin: 16px;
+	padding: 32px 8px;
+	color: #555d66;
 	background-color: white;
-	border-radius: 16px;
+	border-radius: 10px;
 }
 .login div#login > * {
 	width: 100%;
 }
-.login div#login h1 {
-	margin-bottom: 20px;
+.login div#login h1, .login #login_error, .login .message, .login .success {
+	margin: 0 8px 10px!important;
 }
 .login div#login h1 a {
 	overflow: inherit;
 	width: auto;
 	height: auto;
 	margin-bottom: 0;
-	font-size: 36px;
-	color: #000;
+	font-size: 40px;
+	color: #191919;
 	font-weight: bold;
-	text-indent: 0;
-	background-image: none;
 }
 .login #login_error, .login .message, .login .success {
+	padding: 8px!important;
 	border-right: 4px solid #00a0d2;
-	margin-left: 16px!important;
-	margin-right: 16px!important;
-	background-color: #fafafa!important;
+	background-color: #f1f7f9!important;
 	box-shadow: none!important;
+	text-align: center;
 }
 .login #login_error {
 	color: #dc3232;
 	border-right-color: #dc3232;
-	background-color: #ffeeee!important;
+	background-color: #fff6f6!important;
 }
 .login div#login form {
 	margin-top: 0;
-	padding: 16px;
+	padding: 8px;
 	border: 0;
 	box-shadow: none;
 }
 .login div#login form p {
-	float: none;
+	display: block;
+	margin-bottom: 8px;
 }
 .login div#login form input {
-	padding: 10px 32px;
+	margin: 0;
+	padding: 10px 16px;
 	font-size: 16px;
-	text-align: center;
-	background-color: var(--gray-2);
-	border-radius: 64px;
+	background-color: #f1f7f9;
 	border: 0!important;
 	outline: none!important;
 	-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
@@ -89,27 +107,49 @@ background-image: url(<?php echo get_stylesheet_directory_uri().'/assets/images/
 	border: 0!important;
 	outline: none!important;
 }
-.login div#login form p.forgetmenot {
-	margin-bottom: 8px;
+.login .forgetmenot, .login .pw-weak {
+	margin-top: 8px!important;
+}
+.login .forgetmenot label, .login .pw-weak label {
+	margin-bottom: 0!important;
 }
 .login div#login form p.submit input#wp-submit {
 	width: 100%;
 	line-height: 1.5;
 	background-color: #0071a1;
+	transition: .3s;
 }
 .login div#login form p.submit input#wp-submit:hover {
 	background-color: #016087;
 }
 .login div#login p#nav, .login div#login p#backtoblog {
 	width: auto;
-	margin: 16px 0;
-	padding: 0 16px;
+	margin: 0 8px 10px;
+	padding: 0;
 }
 .login #backtoblog a, .login #nav a {
 	box-shadow: none!important;
 }
 .login .privacy-policy-page-link {
 	margin: 0!important;
+}
+
+@media only screen and (min-width: 600px) {
+.login div#login h1, .login #login_error, .login .message, .login .success {
+	margin: 0 16px 16px!important;
+}
+.login #login_error, .login .message, .login .success {
+	padding: 12px!important;
+}
+.login div#login form {
+	padding: 16px;
+}
+.login div#login form p {
+	margin-bottom: 16px;
+}
+.login div#login p#nav, .login div#login p#backtoblog {
+	margin: 0 16px 16px;
+}
 }
 </style>
 <?php
@@ -128,13 +168,13 @@ function doc_register_fields() {
 	?>
 <p>
 	<label for="password">
-		<?php __('Password','doc_text');?>
+		<?php _e('Password','doc-text');?>
 	</label>
 	<input type="password" name="password" id="password" class="input" value="" size="21">
 </p>
 <p>
 	<label for="repeat_password">
-		<?php __('Repeat password','doc_text');?>
+		<?php _e('Repeat password','doc-text');?>
 	</label>
 	<input type="password" name="repeat_password" id="repeat_password" class="input" value="" size="21">
 </p>
