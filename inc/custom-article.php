eb5ac22c47414c5f94012fcc29a2686fd4c9ee8a
wangtingbiao <5114559@qq.com>
2020-11-01 23:26:42 +0800

    Update custom-article.php

--- inc/custom-article.php
+++ inc/custom-article.php
@@ -69,30 +69,7 @@ function get_custom_category_template( $category_template ) {
 	return $category_template;
 }
 }
-
 $cat_template = new Select_Category_Template();
-
-/**
- * The first picture in the article is a thumbnail
- */
-function doc_first_thumbnail() {
-	global $post;
-	$already_has_thumb = has_post_thumbnail( $post->ID );
-	if ( !$already_has_thumb ) {
-		$attached_image = get_children( "post_parent=$post->ID&post_type=attachment&post_mime_type=image&numberposts=1" );
-		if ( $attached_image ) {
-			foreach ( $attached_image as $attachment_id => $attachment ) {
-				set_post_thumbnail( $post->ID, $attachment_id );
-			}
-		}
-	}
-}
-add_action( 'the_post', 'doc_first_thumbnail' );
-add_action( 'save_post', 'doc_first_thumbnail' );
-add_action( 'draft_to_publish', 'doc_first_thumbnail' );
-add_action( 'new_to_publish', 'doc_first_thumbnail' );
-add_action( 'pending_to_publish', 'doc_first_thumbnail' );
-add_action( 'future_to_publish', 'doc_first_thumbnail' );
 
 /**
  * The category loop of the post
