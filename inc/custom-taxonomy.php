52666c2374b3f350809a3eee79aa2f6727199408
wangtingbiao <5114559@qq.com>
2020-11-01 22:17:22 +0800

    Update custom-taxonomy.php

--- inc/custom-taxonomy.php
+++ inc/custom-taxonomy.php
@@ -7,14 +7,14 @@ function custom_taxonomy_label_args( $name, $slugName ) {
 	return $labels = array(
 		'name' => $name,
 		'singular_name' => $slugName,
-		'search_items' => __( 'Search ', 'doc-text' ) . $name,
-		'all_items' => __( 'All ', 'doc-text' ) . $name,
-		'parent_item' => __( 'Parent ', 'doc-text' ) . $name,
-		'parent_item_colon' => __( 'Parent ', 'doc-text' ) . $name,
-		'edit_item' => __( 'Edit ', 'doc-text' ) . $name,
-		'update_item' => __( 'Update ', 'doc-text' ) . $name,
-		'add_new_item' => __( 'Add ', 'doc-text' ) . $name,
-		'new_item_name' => __( 'News ', 'doc-text' ) . $name,
+		'search_items' => __( '搜索', 'doc-text' ) . $name,
+		'all_items' => __( '所有', 'doc-text' ) . $name,
+		'parent_item' => __( '父级', 'doc-text' ) . $name,
+		'parent_item_colon' => __( '父级', 'doc-text' ) . $name,
+		'edit_item' => __( '编辑', 'doc-text' ) . $name,
+		'update_item' => __( '更新', 'doc-text' ) . $name,
+		'add_new_item' => __( '添加', 'doc-text' ) . $name,
+		'new_item_name' => __( '新', 'doc-text' ) . $name,
 		'menu_name' => $name,
 	);
 }
@@ -33,7 +33,7 @@ function custom_taxonomy_args( $name, $slugName, $hierarchical = true, $show_ui 
 function add_custom_taxonomy() {
 	register_taxonomy( 'sites', array( 'site' ),
 		custom_taxonomy_args(
-			__( 'Category', 'doc-text' ), //$name
+			__( '分类', 'doc-text' ), //$name
 			'sites', //$slugName
 			true, //$hierarchical
 			true, //$show_ui
