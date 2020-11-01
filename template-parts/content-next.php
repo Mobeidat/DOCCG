04f6fb81eafe2b8901599b255f4a94bd3aa21b78
wangtingbiao <5114559@qq.com>
2020-11-01 22:17:11 +0800

    Update content-next.php

--- template-parts/content-next.php
+++ template-parts/content-next.php
@@ -8,7 +8,7 @@
  */
 if ( $wp_query->max_num_pages > 1 ) {
 	echo '<div class="site-next">';
-	echo paginate_links( array( 'prev_text' => __( 'Previous', 'doc-text' ), 'next_text' => __( 'Next', 'doc-text' ), 'mid_size' => 1 ) );
+	echo paginate_links( array( 'prev_text' => __( '上一页', 'doc-text' ), 'next_text' => __( '下一页', 'doc-text' ), 'mid_size' => 1 ) );
 	/*if ( !is_paged() ) {
 		next_posts_link( __( 'Next', 'doc-text' ) );
 	} else {}*/
