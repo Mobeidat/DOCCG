55f46bd788db9c92b8f51e6c12cea7463c4c4435
wangtingbiao <5114559@qq.com>
2020-11-04 23:26:16 +0800

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
  */
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<input type="search" name="s" class="searchinput" placeholder="<?php _e( 'Try to search', 'doc-text' );?>" value="<?php the_search_query(); ?>" />
+	<input type="search" name="s" class="searchinput" placeholder="<?php _e( '搜索一下', 'doc-text' );?>" value="<?php the_search_query(); ?>" />
 	<button type="submit" class="searchsubmit"><i class="fa fa-search"></i></button>
 </form>
