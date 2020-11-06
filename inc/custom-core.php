365c330e2ae7e41c2099079297693665ac092ebf
wangtingbiao <5114559@qq.com>
2020-11-06 18:28:45 +0800

    Update custom-core.php

--- inc/custom-core.php
+++ inc/custom-core.php
@@ -4,21 +4,21 @@
  * 后台登录/注册显示修改Logo名称
  * 自定义管理员后台页脚文本
  * 保护您的网站免受恶意请求
- * Only show articles from current user
- * Kill the admin nag
- * Remove nofollow from comments
- * There is only one search result directly enter
- * Remove Query String from Static Resources
- * Remove Emojis
- * Remove Shortlink
- * Disable Embed
- * Disable XML-RPC
- * Remove RSD Link
- * Hide Version
- * Remove WLManifest Link
- * Disable Self Pingback
- * Disable Dashicons in Front-end
- * Disable Contact Form 7 CSS/JS on Every Page
+ * 后台仅显示当前用户的文章
+ * 管理面板的更新提示删除
+ * 从评论中删除nofollow
+ * 搜索结果只有一个直接进入帖子
+ * 从静态资源中删除查询字符串
+ * 删除表情符号
+ * 删除短链接
+ * 禁用embed引用
+ * 禁用XML-RPC服务
+ * 删除RSD链接
+ * 隐藏版本
+ * 删除wlwmanifest链接
+ * 禁止PINGBACK内页
+ * 禁用Dashicons
+ * 禁用联系表7 CSS / JS
  *
  * @package TingBiao Wang
  */
@@ -68,13 +68,13 @@ if ( $user_ID ) {
 }
 
 /**
- * Only show articles from current user
+ * 后台仅显示当前用户的文章
  */
 function doc_check_user_role() {
 	global $current_user;
 	if ( $current_user->roles[ 0 ] != 'administrator' ) {
 
-		// Only show pictures of logged-in users
+		// 仅显示已登录用户的图片
 		add_action( 'pre_get_posts', 'doc_restrict_media_library' );
 
 		function doc_restrict_media_library( $wp_query_obj ) {
@@ -92,7 +92,7 @@ function doc_check_user_role() {
 add_action( 'init', 'doc_check_user_role' );
 
 /**
- * Kill the admin nag
+ * 管理面板的更新提示删除
  */
 if ( !current_user_can( 'edit_users' ) ) {
 	add_action( 'init', create_function( '$a', "remove_action('init', 'wp_version_check');" ), 2 );
@@ -113,7 +113,7 @@ remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
 remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 
 /**
- * Remove nofollow from comments
+ * 从评论中删除nofollow
  */
 function doc_xwp_dofollow( $str ) {
 	$str = preg_replace(
@@ -128,7 +128,7 @@ add_filter( 'comment_reply_link', 'doc_xwp_dofollow' );
 add_filter( 'comment_text', 'doc_xwp_dofollow' );
 
 /**
- * There is only one search result directly enter
+ * 搜索结果只有一个直接进入帖子
  */
 function doc_sin_result() {
 	if ( is_search() ) {
@@ -141,7 +141,7 @@ function doc_sin_result() {
 add_action( 'template_redirect', 'doc_sin_result' );
 
 /**
- * Remove Query String from Static Resources
+ * 从静态资源中删除查询字符串
  */
 function doc_remove_cssjs_ver( $src ) {
 	if ( strpos( $src, '?ver=' ) )
@@ -152,7 +152,7 @@ add_filter( 'style_loader_src', 'doc_remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'doc_remove_cssjs_ver', 10, 2 );
 
 /**
- * Remove Emojis
+ * 删除表情符号
  */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -160,12 +160,12 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 /**
- * Remove Shortlink
+ * 删除短链接
  */
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
 /**
- * Disable Embed
+ * 禁用embed引用
  */
 function doc_disable_embed() {
 	wp_dequeue_script( 'wp-embed' );
@@ -173,27 +173,27 @@ function doc_disable_embed() {
 add_action( 'wp_footer', 'doc_disable_embed' );
 
 /**
- * Disable XML-RPC
+ * 禁用XML-RPC服务
  */
 add_filter( 'xmlrpc_enabled', '__return_false' );
 
 /**
- * Remove RSD Link
+ * 删除RSD链接
  */
 remove_action( 'wp_head', 'rsd_link' );
 
 /**
- * Hide Version
+ * 隐藏版本
  */
 remove_action( 'wp_head', 'wp_generator' );
 
 /**
- * Remove WLManifest Link
+ * 删除wlwmanifest链接
  */
 remove_action( 'wp_head', 'wlwmanifest_link' );
 
 /**
- * Disable Self Pingback
+ * 禁止PINGBACK内页
  */
 function doc_disable_pingback( & $links ) {
 	foreach ( $links as $l => $link )
@@ -203,18 +203,18 @@ function doc_disable_pingback( & $links ) {
 add_action( 'pre_ping', 'doc_disable_pingback' );
 
 /**
- * Disable Dashicons in Front-end
+ * 禁用Dashicons
  */
-function doc_docs_dequeue_dashicon() {
+function doc_dequeue_dashicon() {
 	if ( current_user_can( 'update_core' ) ) {
 		return;
 	}
 	wp_deregister_style( 'dashicons' );
 }
-add_action( 'wp_enqueue_scripts', 'doc_docs_dequeue_dashicon' );
+add_action( 'wp_enqueue_scripts', 'doc_dequeue_dashicon' );
 
 /**
- * Disable Contact Form 7 CSS/JS on Every Page
+ * 禁用联系表7 CSS / JS
  */
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
