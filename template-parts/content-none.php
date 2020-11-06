afb2ea910703b5fde2fbabc9da0e8283f1dc6625
wangtingbiao <5114559@qq.com>
2020-11-06 13:48:54 +0800

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,14 +1,22 @@
 <?php
 /**
- * 没有帖子
+ * 无帖子
  *
- * 没有帖子时，默认使用此列表模板
+ * 根据各个页面查询无结果时显示自定义信息
  *
  * @package TingBiao Wang
  */
 ?>
 <section class="article-box max-width">
 	<p>
-		<?php _e("Sorry, no search criteria are met. Please try some other keywords.","doc-text");?>
+		<?php
+		if ( is_search() ) {
+			__( '抱歉，暂无当前搜索词帖子。请尝试更换其他搜索词。', 'doc-text' );
+		} elseif ( is_single() ) {
+			__( '抱歉，没有相关联的帖子。', 'doc-text' );
+		} else {
+			__( '抱歉，没有符合条件帖子。', 'doc-text' );
+		}
+		?>
 	</p>
 </section>
